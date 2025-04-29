03d7a866155b9964f073d1d71fb63db2b1e3e5e5
CleytonSales <cleyton.prudencio@ebix.com>
2025-04-29 14:24:32 -0300

    remocao pastga indesejada

--- src/main/java/com/system/watchCar/repository/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.system.watchCar.repository;
-
-import com.system.watchCar.entity.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
