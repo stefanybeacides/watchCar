60db26ffc26768684fa9ab74786f3136b3265c76
Cleyton <cleyton.prudencio@grupoimg.com.br>
2025-05-07 18:05:36 -0300

    AJuste frontend deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,14 +7,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  preview: {
+    host: '0.0.0.0', // Permite acessar de qualquer IP
+    allowedHosts: ['watchcar.onrender.com'], // Permite esse domínio específico
   },
 })
