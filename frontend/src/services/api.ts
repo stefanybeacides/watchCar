76cb6564283b9821231b40d20c54066c2c639803
Cleyton <cleyton.prudencio@grupoimg.com.br>
2025-05-08 10:01:22 -0300

    Ajustes de conexao de dados front e back.

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // base da sua API
+  baseURL: import.meta.env.VITE_API_BASE_URL, // base da sua API
   headers: {
     'Content-Type': 'application/json',
   },
