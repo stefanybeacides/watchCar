242c4e36442b070dd2a3b6c305e187ac41442714
Cleyton <cleyton.prudencio@grupoimg.com.br>
2025-05-08 00:54:55 -0300

    removendo icones de todos lugares

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
 import './assets/main.css'
 import 'vue3-toastify/dist/index.css'
-import '@fortawesome/fontawesome-free/css/all.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Vue3Toastify from 'vue3-toastify'
@@ -8,8 +7,6 @@ import App from './App.vue'
 import router from './router'
 import 'vue3-toastify/dist/index.css' // Importe o CSS do Toastify
 // Importando Font Awesome
-import { faCar } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // Adicionando o ícone `faCar` à biblioteca
 
@@ -26,5 +23,4 @@ app.use(Vue3Toastify, {
 
 app.use(createPinia())
 app.use(router)
-app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
